e0e6db86931772fbd7de2bcb3a3036284bc29d6b
balath <sflor5@alumno.uned.es>
2024-03-21 10:54:59 +0100

    add semiphrase ending control

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -3,19 +3,21 @@ package model
 import cats.kernel.Previous
 import dsl.{ChordFigure, Mode, Note}
 
-import scala.util.Random
+import scala.util.{Failure, Random, Success, Try}
 
-type ChainElement = (Double, ChordFigure)
-type Distribution = Vector[ChainElement]
-type FirstOrderChain = Map[ChordFigure, Distribution]
-type SecondOrderChain = Map[ChordFigure, Map[ChordFigure, Distribution]]
+type SelectionTuple = (Double, ChordFigure)
+type SelectionWheel = Vector[SelectionTuple]
+type FirstOrderTransitions = Map[ChordFigure, SelectionWheel]
+type SecondOrderTransitions = Map[ChordFigure, Map[ChordFigure, SelectionWheel]]
 
-case class ChoralsMetadata(parsedChorals: Int, keyAndModeMap: Map[(Note, Mode), Int])
+case class ChoralesMetadata(parsedChorals: Int, firstSemiphraseLength: Double)
 
 case class Model(
-                  cf1Distribution: Distribution,
-                  cf2Transitions: FirstOrderChain,
-                  initialSemiphraseTransitions: SecondOrderChain
+                  cf1Distribution: SelectionWheel,
+                  cf2Transitions: FirstOrderTransitions,
+                  initialSemiphraseTransitions: SecondOrderTransitions,
+                  initialSemiphraseEndingChords: Map[ChordFigure, Double],
+                  metadata: ChoralesMetadata
                   //                  middles: TransitionMatrix,
                   //                  last: TransitionMatrix
                 ):
@@ -26,18 +28,32 @@ case class Model(
     val cf2Distribution = cf2Transitions(cf1)
     cf2Distribution.find((prob, chord) => r.nextDouble() <= prob).getOrElse(cf2Distribution.last)._2
 
+  def endSemiphrase(currentLength: Int, currentChordEndingProbability: Double, r: Random): Boolean =
+    val currentLengthBias = (currentLength / metadata.firstSemiphraseLength) * currentChordEndingProbability
+    val random = r.nextDouble()
+    println(s"($currentLength): chordProb = $currentChordEndingProbability - bias = $currentLengthBias - r = $random")
+    if currentChordEndingProbability == 1.0 then return true
+    if random < currentLengthBias then return true
+    false
+
   def genInitialSemiphrase(r: Random): Vector[ChordFigure] =
-    def rec(previousChords: (ChordFigure, ChordFigure), acc: Vector[ChordFigure],end: Int): Vector[ChordFigure] = end match
-      case 0 => acc
-      case _ => 
-        val nextChordDistribution = initialSemiphraseTransitions(previousChords._1)(previousChords._2)
-        val nextChord = nextChordDistribution.find((prob, chord) => r.nextDouble() <= prob).getOrElse(nextChordDistribution.last)._2
-        rec((previousChords._2, nextChord), acc :+ nextChord, end - 1)
-    
+    def rec(previousChords: (ChordFigure, ChordFigure), acc: Vector[ChordFigure], end: Boolean): Vector[ChordFigure] = end match
+      case true => acc
+      case false =>
+        val nextChordDistribution: Try[SelectionWheel] = Try(initialSemiphraseTransitions(previousChords._1)(previousChords._2))
+        nextChordDistribution match
+          case Failure(e) =>
+            println(s"Ending by failure: ${e.getMessage}")
+            acc
+          case Success(nCD) =>
+            val nextChord = nCD.find((prob, chord) => r.nextDouble() <= prob).getOrElse(nCD.last)._2
+            print(s"Ending with $nextChord ")
+            val endNow = endSemiphrase(acc.size + 1, initialSemiphraseEndingChords.getOrElse(nextChord, 0.0), r)
+            rec((previousChords._2, nextChord), acc :+ nextChord, endNow)
+
+    println(s"First semiphrase length = ${metadata.firstSemiphraseLength}")
     val cf1 = genFirstChord(r)
     val cf2 = genSecondChord(cf1)(r)
-    rec((cf1, cf2), Vector(cf1,cf2),10)
-
-
+    rec((cf1, cf2), Vector(cf1, cf2), false)
 
 end Model
--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -3,9 +3,9 @@ package generator
 import cats.effect.{ExitCode, IO, IOApp, Resource}
 
 import scala.util.{Failure, Random, Success, Try, control}
-import common.DataRegex.*
-import dsl.*
-import model.{Distribution, FirstOrderChain, Model, SecondOrderChain}
+import common.DataRegex._
+import dsl._
+import model._
 
 object Parser extends IOApp:
 
@@ -15,15 +15,15 @@ object Parser extends IOApp:
 
   val program = for {
     lines <- readFromRawData(dataPath)
-    chorals <- extractChorals(lines)
-    //    metadata <- processMetadata(chorals)
-    model <- processModel(chorals)
-    _ <- IO.println(s"${chorals.size} chorals parsed successfully")
-      >> IO.println(model.cf1Distribution.mkString("First------\n","\n","\n-------------"))
-      >> IO.println(model.cf2Transitions.mkString("Second------\n","\n","\n-------------"))
-      >> IO.println("\nTransitions----------------\n")
-      >> IO.println(model.initialSemiphraseTransitions.map((cf,mp)=> s"\n$cf ------ \n${mp.mkString("\n")}"))
-      >> IO.println(s"Semiphrase simulation: ${model.genInitialSemiphrase(r)}")
+    chorales <- extractChorales(lines)
+    (majorChorales, minorChorales): (Vector[Choral],Vector[Choral]) = chorales.partition(_.mode.equals(Mode.maj))
+    majorMetadata <- processMetadata(majorChorales)
+    minorMetadata <- processMetadata(minorChorales)
+    majorModel <- processModel(majorChorales, majorMetadata)
+    minorModel <- processModel(minorChorales, minorMetadata)
+    _ <- IO.println(s"${chorales.size} chorals parsed successfully")
+      >> IO.println(s"Major semiphrase simulation: ${majorModel.genInitialSemiphrase(r)}")
+      >> IO.println(s"Minor semiphrase simulation: ${minorModel.genInitialSemiphrase(r)}")
   } yield ExitCode.Success
 
   def readFromRawData(path: String): IO[Vector[String]] =
@@ -31,8 +31,8 @@ object Parser extends IOApp:
       .fromAutoCloseable(IO(scala.io.Source.fromFile(path)))
       .use(reader => IO(reader.getLines.toVector))
 
-  def extractChorals(lines: Vector[String]): IO[Vector[Choral]] =
-    val chorals: Vector[Choral] = lines.map(line =>
+  def extractChorales(lines: Vector[String]): IO[Vector[Choral]] =
+    val chorales: Vector[Choral] = lines.map(line =>
       val (metadata, chords) = line match {
         case chordAndMetadataRegex(m, c) => (m, c)
       }
@@ -62,58 +62,73 @@ object Parser extends IOApp:
 
       Choral(choralNum, key, mode, firstSemiphrase, middleSemiphrases, lastSemiphrase)
     )
-    IO(chorals)
+    IO(chorales)
+    
+  def processMetadata(chorales: Vector[Choral]): IO[ChoralesMetadata] =
+    val firstSemiphraseLength = chorales.map(_.first).foldLeft(0)((z,sp) => z + sp.size) / (1.0 * chorales.size)
+    IO(ChoralesMetadata(chorales.size, firstSemiphraseLength))
 
   //Model processing functions ---------------------------------------------------------------------------------
-  def processModel(chorals: Vector[Choral]): IO[Model] =
-    val modelSize = chorals.size
-    val firstSemiphrases = chorals.map(_.first)
-    val middleSemiphrases = chorals.flatMap(_.middle)
-    val lastSemiphrases = chorals.map(_.last)
+  def processModel(chorales: Vector[Choral], metadata: ChoralesMetadata): IO[Model] =
+    val firstSemiphrases = chorales.map(_.first)
+    val middleSemiphrases = chorales.flatMap(_.middle)
+    val lastSemiphrases = chorales.map(_.last)
 
-    val cf1Probs: Vector[(ChordFigure, Double)] = firstSemiphrases
-      .map(_.head)
-      .groupMapReduce(identity)(_ => 1.0)(_ + _)
-      .toVector
-    val cf1CumulativeDistribution: Distribution = cumulateProbabilities(cf1Probs, modelSize)
+    val cf1Ocurrences = firstSemiphrases.map(_.head).groupMapReduce(identity)(_ => 1.0)(_ + _)
+    val cf1SelectionWheel: SelectionWheel = chordOcurrencesToSelectionWheel(cf1Ocurrences.toVector, firstSemiphrases.size)
 
-    val cf2CumulativeDistribution: FirstOrderChain = firstSemiphrases
+    val cf2Transitions: FirstOrderTransitions = firstSemiphrases
       .map(sp => (sp(0), sp(1)))
       .groupMapReduce((cf1, _) => cf1)((_, cf2) => Vector(cf2))(_ ++ _).toVector
-      .map((cf1, cf2s) =>
-        val cf2sProbs = cf2s.groupMapReduce(identity)(_ => 1.0)(_ + _).toVector
-        val cf2sCumDistrib = cumulateProbabilities(cf2sProbs, cf2s.size)
-        cf1 -> cf2sCumDistrib
-      ).toMap
+      .map(chordProgressionsToTransitions).toMap
 
-    val initialSemiphraseTransitions: SecondOrderChain = firstSemiphrases
+    val firstSemiphraseTransitions: SecondOrderTransitions = firstSemiphrases
       .flatMap(sp => sp.lazyZip(sp.tail).lazyZip(sp.tail.tail))
-      .groupMapReduce((cf1,_,_) => cf1)((_,cf2,cf3) => Vector((cf2,cf3)))(_ ++ _).toVector
-      .map((cf1,cfs) =>
+      .groupMapReduce((cf1, _, _) => cf1)((_, cf2, cf3) => Vector((cf2, cf3)))(_ ++ _).toVector
+      .map((cf1, cfs) =>
         val subMap = cfs
           .groupMapReduce((cf2, _) => cf2)((_, cf3) => Vector(cf3))(_ ++ _).toVector
-          .map((cf2, cf3s) =>
-            val cf3sProbs = cf3s.groupMapReduce(identity)(_ => 1.0)(_ + _).toVector
-            val cf3sCumDistrib = cumulateProbabilities(cf3sProbs, cf3s.size)
-            cf2 -> cf3sCumDistrib
-          ).toMap
+          .map(chordProgressionsToTransitions).toMap
         cf1 -> subMap
       ).toMap
 
+    val endingChordsProbabilities: Map[ChordFigure, Double] = firstSemiphrases
+      .map(_.last)
+      .groupMapReduce(identity)(_ => 1.0)(_ + _)
+      .map((cf,occurrences) => (cf, occurrences / firstSemiphrases.size))
 
-    //    val firstSemiphraseTransitionMatrix = generateTransitionMatrixFromSemiphrases(chorals.map(_.first))
-    //    val middleSemiphrasesTransitionMatrix = generateTransitionMatrixFromSemiphrases(chorals.flatMap(_.middle))
-    //    val lastSemiphraseTransitionMatrix = generateTransitionMatrixFromSemiphrases(chorals.map(_.last))
-    IO(Model(cf1CumulativeDistribution, cf2CumulativeDistribution, initialSemiphraseTransitions))
+    println(endingChordsProbabilities)
+    IO(Model(cf1SelectionWheel, cf2Transitions, firstSemiphraseTransitions, endingChordsProbabilities, metadata))
 
-  def cumulateProbabilities(probabilitiesVector: Vector[(ChordFigure, Double)], modelSize: Int): Vector[(Double, ChordFigure)] =
-    def rec(lastValue: Double, acc: Vector[(Double, ChordFigure)], tail: Vector[(ChordFigure, Double)]): Vector[(Double, ChordFigure)] = tail match
-      case (cf, q) +: newTail =>
-        val newValue = lastValue + (q / modelSize)
-        rec(newValue, acc :+ (newValue, cf), newTail)
-      case _ => acc
+  /**
+   * Given a tuple of a chord and its next chords in a progression, generates the transitions tuple for such chord
+   * by counting ocurrences and generating a selection wheel.   *
+   * @param progressions tuple of a chord and a collection of its possible next chords
+   * @return tuple of a chord and its selection wheel of transitions
+   */
+  def chordProgressionsToTransitions(progressions: (ChordFigure, Vector[ChordFigure])): (ChordFigure, SelectionWheel) =
+    val cf2sOccurrences: Vector[(ChordFigure, Double)] = progressions._2.groupMapReduce(identity)(_ => 1.0)(_ + _).toVector
+    val cf2sSelectionWheel: SelectionWheel = chordOcurrencesToSelectionWheel(cf2sOccurrences, progressions._2.size)
+    progressions._1 -> cf2sSelectionWheel
 
-    rec(0.0f, Vector.empty, probabilitiesVector)
+  /**
+   * Given a vector of chords figures with its occurrences, generates a "roulette wheel" as used in Fitness
+   * Proportionate Selection algorithm (FPS), used in genetic algorithms for selecting chromosomes.
+   * * There are faster alternatives, as "alias method"
+   *
+   * @param chordOccurrences Vector of tuples with a chord and its occurrences
+   * @param totalOccurrences Sum of all chords occurrences
+   * @return A roulette wheel with the chord proportionate distribution in a range [0,~1)
+   */
+  def chordOcurrencesToSelectionWheel(chordOccurrences: Vector[(ChordFigure, Double)], totalOccurrences: Int): SelectionWheel =
+    def rec(previousMark: Double, acc: Vector[(Double, ChordFigure)], chordsOccurrences: Vector[(ChordFigure, Double)]): SelectionWheel =
+      chordsOccurrences match
+        case (cf, occurrences) +: tail =>
+          val newMark: Double = previousMark + (occurrences / totalOccurrences)
+          rec(newMark, acc :+ (newMark, cf), tail)
+        case _ => acc
+
+    rec(0.0f, Vector.empty, chordOccurrences)
 
 
 
