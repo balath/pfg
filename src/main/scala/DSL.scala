127db41e5c20b1c6d1e58369c605ce216bd0e848
balath <sflor5@alumno.uned.es>
2024-04-20 09:38:44 +0200

    Chord creation from ChordFigure added

--- src/main/scala/DSL.scala
+++ src/main/scala/DSL.scala
@@ -1,16 +1,23 @@
 package dsl
 
-import common.DataRegex._
+import common.DataRegex.*
+
+import scala.util.{Failure, Success, Try}
 
 type Semiphrase = Vector[ChordFigure]
+
 case class Choral(num: Int, key: Note, mode: Mode, semiphrases: Vector[Semiphrase])
-case class Chord(bass: Note, notes: Vector[Note])
+
+case class Chord(bass: Note, notes: Vector[Note]):
+  override def toString: String = s"$bass, ${notes.mkString(", ")}"
+
 enum Mode:
   case maj extends Mode
   case min extends Mode
-end Mode 
+end Mode
+
 /**
- *  Musical notes are named according to the LylyPond standard for english (https://lilypond.org/):
+ * Musical notes are named according to the LylyPond standard for english (https://lilypond.org/):
  *  - "a" to "g" for pitch
  *  - "s" for sharp, "f" for flat, "x" for double sharp and "ff" for double flat
  */
@@ -53,30 +60,87 @@ enum Note(val pitch: Int):
 
   val diatonicScaleNumNote: Map[Int, String] = Map(0 -> "a", 1 -> "b", 2 -> "c", 3 -> "d", 4 -> "e", 5 -> "f", 6 -> "g")
   val diatonicScaleNoteNum: Map[String, Int] = Map("a" -> 0, "b" -> 1, "c" -> 2, "d" -> 3, "e" -> 4, "f" -> 5, "g" -> 6)
+
   def interval(interval: Interval): Note =
     val newPitch = (this.pitch + interval.semitones) % 12
     val newNoteKey = (diatonicScaleNoteNum(this.toString.substring(0, 1)) + interval.diatonic - 1) % 7
     val newNoteName: String = diatonicScaleNumNote(newNoteKey)
     val newPitchNotes = Note.values.filter(_.pitch == newPitch)
     newPitchNotes.filter(_.toString.startsWith(newNoteName)).headOption.getOrElse(newPitchNotes.head)
+
   def chord(chord: ChordFigure): Chord =
-    val grade, kind, inversion, baseGrade = chord.toString match { case chordRegex(g, k, i, b) => (g, k, i, b) }
-    Chord(Note.c, Vector.empty)
+    import Interval._, Mode._
+    val (grade, kind, inversion, base) = chord.toString match
+      case chordRegex(g, k, i, b) => (Grade.valueOf(g), Try(Kind.valueOf(k)), Try(i.toInt), Try(Grade.valueOf(b)))
+//    println(s"Grade = $grade, Kind = $kind, Inversion = $inversion, Base = $base")
+    val tonic = if base.isSuccess then this.interval(base.get.interval) else this
+    val third = if grade.mode.equals(min) then tonic.interval(min3) else tonic.interval(maj3)
+    val fifth = kind match
+      case Failure(_) => tonic.interval(perf5)
+      case Success(Kind.ø) | Success(Kind.o) => tonic.interval(dis5)
+      case Success(Kind.aug) => tonic.interval(aug5)
+    val treble = if kind.getOrElse(Kind.o).equals(Kind.ø) then tonic.interval(min7)
+    else
+      if inversion.isFailure then tonic
+      else inversion.get match
+      case 6 | 64 => tonic
+      case 7 | 65 | 43 | 42 =>
+        if kind.getOrElse(Kind.ø).equals(Kind.o) then tonic.interval(dis7)
+        else if grade.mode.equals(min) || grade.equals(Grade.V) || grade.equals(Grade.bVII) then tonic.interval(min7)
+        else tonic.interval(maj7)
+    inversion match
+      case Failure(_) | Success(7) => Chord(tonic, Vector(third, fifth, treble))
+      case Success(6) | Success(65) => Chord(third, Vector(fifth, treble, tonic))
+      case Success(64) | Success(43) => Chord(fifth, Vector(treble, tonic, third))
+      case Success(42) => Chord(treble, Vector(tonic, third, fifth))
 
 end Note
+
+enum Kind:
+  case ø extends Kind
+  case o extends Kind
+  case aug extends Kind
+
+
+enum Grade(val interval: Interval, val mode: Mode):
+  case I extends Grade(Interval.unis, Mode.maj)
+  case i extends Grade(Interval.unis, Mode.min)
+  case bII extends Grade(Interval.min2, Mode.maj)
+  case II extends Grade(Interval.maj2, Mode.maj)
+  case ii extends Grade(Interval.maj2, Mode.min)
+  case bIII extends Grade(Interval.min3, Mode.maj)
+  case iii extends Grade(Interval.maj3, Mode.min)
+  case IV extends Grade(Interval.perf4, Mode.maj)
+  case iv extends Grade(Interval.perf4, Mode.min)
+  case V extends Grade(Interval.perf5, Mode.maj)
+  case v extends Grade(Interval.perf5, Mode.min)
+  case bVI extends Grade(Interval.min6, Mode.maj)
+  case vi extends Grade(Interval.maj6, Mode.min)
+  case bVII extends Grade(Interval.min7, Mode.maj)
+  case bvii extends Grade(Interval.min7, Mode.min)
+  case vii extends Grade(Interval.maj7, Mode.min)
+end Grade
+
+val a: Note = Note.b
+
 enum Interval(val diatonic: Int, val semitones: Int):
+  case unis extends Interval(0, 0)
   case min2 extends Interval(2, 1)
   case maj2 extends Interval(2, 2)
   case min3 extends Interval(3, 3)
   case maj3 extends Interval(3, 4)
+  case perf4 extends Interval(4, 5)
   case dis5 extends Interval(5, 6)
   case perf5 extends Interval(5, 7)
   case aug5 extends Interval(5, 8)
+  case min6 extends Interval(6, 8)
+  case maj6 extends Interval(6, 9)
   case dis7 extends Interval(7, 9)
   case min7 extends Interval(7, 10)
-  case maj7 extends Interval(7,11)
-  case perf8 extends Interval(1,0)
+  case maj7 extends Interval(7, 11)
+  case perf8 extends Interval(8, 12)
 end Interval
+
 enum ChordFigure:
   case Empty extends ChordFigure
   case I extends ChordFigure
@@ -298,4 +362,79 @@ enum ChordFigure:
   case viio6_bIII extends ChordFigure
   case iiø42_iv extends ChordFigure
   case viio7_iv extends ChordFigure
+  case bIII43 extends ChordFigure
+  case bIIIaug43_iv extends ChordFigure
+  case bVI_bVII extends ChordFigure
+  case bVII7_bVII extends ChordFigure
+  case vii64 extends ChordFigure
+  case v_bvii extends ChordFigure
+  case bvii6 extends ChordFigure
+  case viio7_bvii extends ChordFigure
+  case bvii extends ChordFigure
+  case vi_bVI extends ChordFigure
+  case bVI_bvii extends ChordFigure
+  case bVII7_bvii extends ChordFigure
+  case V65_bvii extends ChordFigure
+  case iio6_v extends ChordFigure
+  case bIIIaug6_v extends ChordFigure
+  case ii7_IV extends ChordFigure
+  case IV6_bVII extends ChordFigure
+  case ii65_bVII extends ChordFigure
+  case viiø43_IV extends ChordFigure
+  case viio6_IV extends ChordFigure
+  case viio64_IV extends ChordFigure
+  case iiø42_ii extends ChordFigure
+  case bIIIaug42 extends ChordFigure
+  case V65_II extends ChordFigure
+  case viø42_v extends ChordFigure
+  case iiø_II extends ChordFigure
+  case V7_II extends ChordFigure
+  case II extends ChordFigure
+  case iiø43_v extends ChordFigure
+  case ii7_ii extends ChordFigure
+  case viio_v extends ChordFigure
+  case viø65_v extends ChordFigure
+  case vi_V extends ChordFigure
+  case IV7_V extends ChordFigure
+  case viø_vi extends ChordFigure
+  case iiø42_vi extends ChordFigure
+  case I42 extends ChordFigure
+  case iii64 extends ChordFigure
+  case viiø_V extends ChordFigure
+  case V42_vi extends ChordFigure
+  case V43_ii extends ChordFigure
+  case viiø42_V extends ChordFigure
+  case vio_v extends ChordFigure
+  case viiø43 extends ChordFigure
+  case ii43 extends ChordFigure
+  case viio64 extends ChordFigure
+  case iio6_vi extends ChordFigure
+  case viio7_iii extends ChordFigure
+  case i7 extends ChordFigure
+  case i43 extends ChordFigure
+  case iio extends ChordFigure
+  case viiø42_bVI extends ChordFigure
+  case vi42 extends ChordFigure
+  case I43 extends ChordFigure
+  case iii43_V extends ChordFigure
+  case viiø65_V extends ChordFigure
+  case bIII_ii extends ChordFigure
+  case bVII_ii extends ChordFigure
+  case iio6_ii extends ChordFigure
+  case viiø_v extends ChordFigure
+  case viiø_bVI extends ChordFigure
+  case V43_vi extends ChordFigure
+  case bVI_ii extends ChordFigure
+  case iiø_vi extends ChordFigure
+  case IV43 extends ChordFigure
+  case I42_V extends ChordFigure
+  case vi7_V extends ChordFigure
+  case ii64_V extends ChordFigure
+  case iii6_V extends ChordFigure
+  case IV6_V extends ChordFigure
+  case iiø65_ii extends ChordFigure
+  case vi43 extends ChordFigure
+  case v64_ii extends ChordFigure
+  case IV6_IV extends ChordFigure
+
 end ChordFigure
