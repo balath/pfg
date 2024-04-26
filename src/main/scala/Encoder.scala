d72fa665eafe7a38824c41d7fc19ee4493f4ca8c
balath <sflor5@alumno.uned.es>
2024-04-26 10:19:53 +0200

    fixing midi output 5

--- src/main/scala/Encoder.scala
+++ src/main/scala/Encoder.scala
@@ -40,7 +40,7 @@ def encodeToLilypond(choral: GeneratedChoral): String =
        |  }
        |  \\layout {}
        |  \\midi {
-       |     \\tempo 4 = 120
+       |     \\tempo 4 = 60
        |  }
        |}
   """.stripMargin
